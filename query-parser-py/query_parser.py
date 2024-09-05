1b530066ba182a40fbd06221ed5bd1c0b8e1c73b
Dmitriy Brusenin <d.brusenin@vk.team>
2024-09-06 02:36:21 +0300

    support json escaping & v2 parse function with error dynamic alloc & sync sources

--- query-parser-py/query_parser.py
+++ query-parser-py/query_parser.py
@@ -11,6 +11,10 @@ lib.parse_query.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
 lib.parse_query.restype = ctypes.c_char_p
 lib.free_ast.argtypes = [ctypes.c_char_p]
 
+lib.parse_query_v2.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_char_p)]
+lib.free_ast_v2.argtypes = [ctypes.c_char_p]
+lib.free_error_v2.argtypes = [ctypes.c_char_p]
+
 
 class ParseError(Exception):
     pass
@@ -18,12 +22,12 @@ class ParseError(Exception):
 
 @contextmanager
 def query_AST(query: str):
-    ast_json = parse_query(query)
+    ast_json = parse_query_v2(query)
     ast = json.loads(ast_json)
     try:
         yield ast
     finally:
-        lib.free_ast(ast_json)
+        lib.free_ast_v2(ast_json)
 
 
 def parse_query(query: str) -> bytes:
@@ -32,6 +36,27 @@ def parse_query(query: str) -> bytes:
     if len(error_msg.value) > 0:
         raise ParseError(error_msg.value.decode('utf-8'))
     return ast_json
+
+
+def parse_query_v2(query: str) -> bytes:
+    ast_json = ctypes.c_char_p()
+    error_msg = ctypes.c_char_p()
+    lib.parse_query_v2(ctypes.c_char_p(query.encode('utf-8')), ctypes.byref(ast_json), ctypes.byref(error_msg))
+
+    if error_msg.value is not None:
+        msg = error_msg.value.decode('utf-8')
+        lib.free_error_v2(error_msg)
+        raise ParseError(msg)
+    assert ast_json.value is not None
+    return ast_json.value
+
+
+def free_ast(ast_json: bytes):
+    lib.free_ast(ast_json)
+
+
+def free_ast_v2(ast_json: bytes):
+    lib.free_ast_v2(ast_json)
 
 
 if __name__ == '__main__':
