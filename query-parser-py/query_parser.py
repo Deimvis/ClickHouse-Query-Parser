a272e24ad245b6983f572f8fe49e67af98e20987
Dmitriy Brusenin <d.brusenin@vk.team>
2024-12-09 10:41:32 +0300

    rename exported C functions to avoid collisions

--- query-parser-py/query_parser.py
+++ query-parser-py/query_parser.py
@@ -8,13 +8,13 @@ from typing import Generator, Dict, List, Union
 
 QUERY_PARSER_SO_LIB = os.getenv('QUERY_PARSER_SO_LIB')
 lib = ctypes.CDLL(QUERY_PARSER_SO_LIB)
-lib.parse_query.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-lib.parse_query.restype = ctypes.c_char_p
-lib.free_ast.argtypes = [ctypes.c_char_p]
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_parse_query.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_parse_query.restype = ctypes.c_char_p
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_ast.argtypes = [ctypes.c_char_p]
 
-lib.parse_query_v2.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_char_p)]
-lib.free_ast_v2.argtypes = [ctypes.c_char_p]
-lib.free_error_v2.argtypes = [ctypes.c_char_p]
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_parse_query_v2.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_char_p)]
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_ast_v2.argtypes = [ctypes.c_char_p]
+lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_error_v2.argtypes = [ctypes.c_char_p]
 
 
 class ParseError(Exception):
@@ -29,12 +29,12 @@ def query_AST(query: str) -> Generator[JSON, None, None]:
     try:
         yield ast
     finally:
-        lib.free_ast_v2(ast_json)
+        lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_ast_v2(ast_json)
 
 
 def parse_query(query: str) -> ctypes.c_char_p:
     error_msg = ctypes.create_string_buffer(1024)
-    ast_json = lib.parse_query(ctypes.c_char_p(query.encode('utf-8')), error_msg)
+    ast_json = lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_parse_query(ctypes.c_char_p(query.encode('utf-8')), error_msg)
     if len(error_msg.value) > 0:
         raise ParseError(error_msg.value.decode('utf-8'))
     return ast_json
@@ -43,22 +43,22 @@ def parse_query(query: str) -> ctypes.c_char_p:
 def parse_query_v2(query: str) -> ctypes.c_char_p:
     ast_json = ctypes.c_char_p()
     error_msg = ctypes.c_char_p()
-    lib.parse_query_v2(ctypes.c_char_p(query.encode('utf-8')), ctypes.byref(ast_json), ctypes.byref(error_msg))
+    lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_parse_query_v2(ctypes.c_char_p(query.encode('utf-8')), ctypes.byref(ast_json), ctypes.byref(error_msg))
 
     if error_msg.value is not None:
         msg = error_msg.value.decode('utf-8')
-        lib.free_error_v2(error_msg)
+        lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_error_v2(error_msg)
         raise ParseError(msg)
     assert ast_json.value is not None
     return ast_json
 
 
 def free_ast(ast_json: ctypes.c_char_p):
-    lib.free_ast(ast_json)
+    lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_ast(ast_json)
 
 
 def free_ast_v2(ast_json: ctypes.c_char_p):
-    lib.free_ast_v2(ast_json)
+    lib.chqp_2b52ae1fb9f4ec8c46b8c527df829c25_free_ast_v2(ast_json)
 
 
 if __name__ == '__main__':
